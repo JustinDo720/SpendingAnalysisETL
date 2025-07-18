89d435ff6031f8cdee2c8272a079cfd860ac301b
Justin Do <56330379+JustinDo720@users.noreply.github.com>
2025-07-18 13:51:24 -0400

    Initial FastAPI Template for ETL

---
+++ README.md
@@ -0,0 +1,10 @@
+# Spending Analysis ETL
+
+## Primary Function 
+
+Microservice to **extract** data from our Django Restapi, **transform** our data (maybe include Vertex AI for more insightful data) and **load** into *Snowflake*.
+
+## Development 
+
+**07/18/25**
+- Building an **extractor** to **ingest** data from our Django RestAPI
---
+++ extractor.py
@@ -0,0 +1,43 @@
+import requests 
+from typing import Dict, Any, List 
+
+"""
+get_uploaded_files()
+    - Find all the uploaded files 
+
+get_summary(upload_id)
+    - Access the summary endpoint for each 
+
+
+Local Endpoints:
+    - All Uploaded Files:
+        - http://127.0.0.1:8000/uploads/
+    - Summary Endpoint:
+        - http://127.0.0.1:8000/uploads/<int:upload_id>/summary/
+"""
+
+BASE_URL = 'http://127.0.0.1:8000'
+
+def get_summary(upload_id: int) -> Dict[str, Any]:
+    try:
+        r = requests.get(BASE_URL + f'/uploads/{upload_id}/summary')
+        r.raise_for_status()
+
+        # If our request went through...
+        if r.status_code == 200:
+            return r.json()
+    except Exception as e:
+        print(f'Failed to fetch summary for upload ID: {upload_id}')
+        return {} 
+
+def get_uploaded_files() -> List[int]:
+    try:
+        r = requests.get(BASE_URL + '/uploads/')
+        r.raise_for_status() 
+        data = r.json() 
+
+        uploaded_files_id = [d['id'] for d in data]
+        return uploaded_files_id
+    except requests.RequestException as e:
+        print(f'Failed to fetch uploaded files...')
+        return {} 
